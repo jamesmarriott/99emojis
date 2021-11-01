b7b5234791dc0e2f6214434ac8bed092ee2ea3ca
James Marriott <sawheadproductions@gmail.com>
2021-11-01 09:06:53 +0100

    updates

--- src/pages/gameplay.js
+++ src/pages/gameplay.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import QuestionDisplay from "../components/questionDisplay"
 import CountModal from "../components/countModal"
 import useSound from 'use-sound'
-import img from '../opimage.png';
 import sfxbeeplow from '../sfx/beeplow.mp3'
 import sfxbeephigh from '../sfx/beephigh.mp3'
 
@@ -60,24 +59,25 @@ const [beepHigh] = useSound(
                 <CountModal countDown={countDown}/> :
                 <>
                 <h1 className="text-4xl font-bold text-center leading-none 
-                mb-2">😂 {emojiAmount - 1} Emojis 🤦</h1>
+                mb-2">Flag Match</h1>
                 <h2 className="text-2xl text-center leading-none 
-                mb-2 mt-3 pb-5">(and a different one)</h2>
+                mb-2 mt-3 pb-5">Learn the flags of the world!</h2>
+                
                 <select
                 defaultValue={emojiAmount}
                 onChange={handleChange}
                 className="w-full bg-white border border-gray-00 hover:border-gray-500 px-2 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline text-2xl">
                     <option value="100" 
                     className="text-2xl text-center leading-none
-                    mb-2">99+1 (hard)</option>
+                    mb-2">Europe</option>
 
-                    <option value="81"
+                    {/* <option value="81"
                     className="text-2xl text-center leading-none
                     mb-2">80+1 (easier - tablet friendly)</option>
 
                     <option value="64"
                     className="text-2xl text-center leading-none
-                    mb-2">63+1 (easiest - mobile friendly)</option>
+                    mb-2">63+1 (easiest - mobile friendly)</option> */}
 
                   </select>
                 
